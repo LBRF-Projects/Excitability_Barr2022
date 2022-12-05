1a0a1b1eeaab6a093d6a0060148c6590ebcedae4
Austin Hurst <mynameisaustinhurst@gmail.com>
2022-12-05 12:08:28 -0400

    Make animation times a constant 1500ms

--- TraceLab/ExpAssets/Config/TraceLab_independent_variables.py
+++ TraceLab/ExpAssets/Config/TraceLab_independent_variables.py
@@ -8,5 +8,5 @@ TraceLab_ind_vars = IndependentVariableSet()
 
 # Define project variables and variable types
 
-TraceLab_ind_vars.add_variable("animate_time", int, [500, 1000, 1500, 2000, 2500])
+TraceLab_ind_vars.add_variable("animate_time", int, [1500])
 TraceLab_ind_vars.add_variable("figure_name", str, ["random", "template_1477090164.31"])
