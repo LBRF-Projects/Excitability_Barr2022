a47e5d5eb9aeb5e60de73fac39fb87461d78ab6c
Austin Hurst <mynameisaustinhurst@gmail.com>
2022-12-09 14:31:45 -0400

    Add unique repeated shape

--- TraceLab/ExpAssets/Config/TraceLab_independent_variables.py
+++ TraceLab/ExpAssets/Config/TraceLab_independent_variables.py
@@ -9,5 +9,5 @@ TraceLab_ind_vars = IndependentVariableSet()
 # Define project variables and variable types
 
 TraceLab_ind_vars.add_variable("animate_time", int, [1500])
-TraceLab_ind_vars.add_variable("figure_name", str, ["random", "template_1477090164.31"])
+TraceLab_ind_vars.add_variable("figure_name", str, ["random", "bat"])
 TraceLab_ind_vars.add_variable("stim_trial", bool, [True, False])
