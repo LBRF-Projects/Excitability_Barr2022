62ed07dc8a15a3d2d9054b16885343ab5d397cfa
Austin Hurst <mynameisaustinhurst@gmail.com>
2023-01-13 14:14:51 -0400

    Fix occasional TMS init bug

--- TraceLab/ExpAssets/Resources/code/communication.py
+++ TraceLab/ExpAssets/Resources/code/communication.py
@@ -371,6 +371,7 @@ class MagPyController(TMSController):
         err, msg = self._device.highResolutionMode(False, receipt=True)
         if err != 3:
             self._device.setPowerB(0)
+            time.sleep(0.1) # Delay between TMS commands to avoid random errors
             self._device.setPulseInterval(0)
 
     def _set_power(self, level):
